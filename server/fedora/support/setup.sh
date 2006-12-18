612de153de1d017ae1680a101be0d0af083c3767
Joe Presbrey <presbrey@mit.edu>
2006-12-18 13:35:36 +0000

    Legacy-ID: 52

--- server/fedora/support/setup.sh
+++ server/fedora/support/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 yum -y install nagios-plugins nagios-plugins-disk nagios-plugins-users nagios-plugins-procs nagios-plugins-load net-snmp
-rpm -Uvh http://web.mit.edu/afs/athena/contrib/scripts/src/RPMS/x86_64/nagios-nrpe-2.5.1-1.rf.x86_64.rpm
+rpm -Uvh http://scripts.mit.edu/src/RPMS/x86_64/nagios-nrpe-2.5.1-1.rf.x86_64.rpm
