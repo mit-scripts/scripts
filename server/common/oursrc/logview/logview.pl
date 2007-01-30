e4f52894c068dbd54a91023a7cb8a87e94a8bb50
Jeff Arnold <jbarnold@mit.edu>
2007-01-30 07:53:58 +0000

    removed access log code since we have no access logs
    Legacy-ID: 139

--- server/common/oursrc/logview/logview.pl
+++ server/common/oursrc/logview/logview.pl
@@ -3,7 +3,6 @@
 %ENV = ();
 $ENV{'PATH'} = '/bin:/usr/bin';
 my $elogsrc = '/var/log/httpd/error_log';
-#my $alogsrc = '/var/log/httpd/access_log';
 # get by uid the caller's name to find the corresponding locker name
 my $caller = (getpwuid $<)[0];
 $\ = "\n";
@@ -15,10 +14,3 @@ close FOO;
 # Prevent deviousness, like web_scripts directories within web_scripts
 my @el = grep((m|/([^/]+)/web_scripts/| && $caller eq $1), grep(m|/afs/athena.mit.edu/|, @elogs));
 print @el;
-
-#print "--- Access logs for $caller ---";
-#open FOO, $alogsrc;
-#my @alogs = <FOO>;
-#close FOO;
-#my @al = grep(m|\"GET /~$caller|, @alogs);
-#print @al;
