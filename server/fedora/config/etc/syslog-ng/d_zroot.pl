e1a5d45f5b8540fc5096238366daabab7795950f
Anders Kaseorg <andersk@mit.edu>
2008-05-01 12:22:00 +0000

    SHUT.  THE.  FUCK.  UP.
    Legacy-ID: 738

--- server/fedora/config/etc/syslog-ng/d_zroot.pl
+++ server/fedora/config/etc/syslog-ng/d_zroot.pl
@@ -54,6 +54,8 @@ while (1) {
 	} elsif ($message =~ m|^fatal: Read from socket failed: Connection reset by peer$|) {
 	} elsif ($message =~ m|^reverse mapping checking getaddrinfo|) {
 	} elsif ($message =~ m|^pam_succeed_if\(sshd\:auth\)\:|) {
+	} elsif ($message =~ m|^Postponed keyboard-interactive for invalid user |) {
+	} elsif ($message =~ m|^Failed keyboard-interactive/pam for invalid user |) {
 	} else {
 	    sendmsg($message, "scripts-spew");
 	}
