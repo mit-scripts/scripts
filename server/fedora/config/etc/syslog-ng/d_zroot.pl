42c2fe22bfaf274e77654ffa0647bdbb9d9dc197
Geoffrey Thomas <geofft@mit.edu>
2009-01-07 17:46:21 +0000

    Setting tty modes failed: Invalid argument
    Legacy-ID: 939

--- server/fedora/config/etc/syslog-ng/d_zroot.pl
+++ server/fedora/config/etc/syslog-ng/d_zroot.pl
@@ -117,6 +117,7 @@ while (1) {
 	} elsif ($message =~ m|^Address \S+ maps to \S+, but this does not map back to the address|) {
 	} elsif ($message =~ m|^User child is on pid \d+$|) {
 	} elsif ($message =~ m|^Transferred: sent \d+, received \d+ bytes$|) {
+	} elsif ($message =~ m|^Setting tty modes failed: Invalid argument$|) {
 	} else {
 	    sendmsg($message, "scripts-spew");
 	}
