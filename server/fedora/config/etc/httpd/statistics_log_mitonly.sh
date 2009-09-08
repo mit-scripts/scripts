32d3b1484344395a3ddad0d78aa5529bc6dc178d
Quentin Smith <quentin@mit.edu>
2009-09-08 04:54:55 +0000

    Handle ~urls as well when gathering statistics
    Legacy-ID: 1316

--- server/fedora/config/etc/httpd/statistics_log_mitonly.sh
+++ server/fedora/config/etc/httpd/statistics_log_mitonly.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-awk '/^18\./ && ! /^18.181/ { print $2; fflush() }' >> /var/log/httpd/statistics_log
+perl -ne 'BEGIN { $| = 1 }
+next unless /^18\./;
+next if /^18\.181\./;
+chomp; split;
+if ($_[1] eq "scripts.mit.edu" && $_[2] =~ m|/(~[^/]+)/|) {
+print "$1\n";
+} else {
+print "$_[1]\n";
+}' >> /var/log/httpd/statistics_log
+#awk '/^18\./ && ! /^18.181/ { print $2; fflush() }' >> /var/log/httpd/statistics_log
