011df9ced72a86ece4ffc8a97f38c83cb7dc17de
Mitchell Berger <mitchb@mit.edu>
2009-10-03 17:46:12 +0000

    Revert r1330.  Try not to kill *all* of our notifications next time.
    Legacy-ID: 1331

--- server/fedora/config/etc/syslog-ng/d_zroot.pl
+++ server/fedora/config/etc/syslog-ng/d_zroot.pl
@@ -105,7 +105,7 @@ while (1) {
 	} elsif ($message =~ m|^Invalid user|) {
 	} elsif ($message =~ m|^input_userauth_request: invalid user|) {
 	} elsif ($message =~ m|^Received disconnect from|) {
-	} elsif ($message =~ m|^(Postponed|Failed) keyboard-interactive|) {
+	} elsif ($message =~ m|^Postponed keyboard-interactive|) {
 	} elsif ($message =~ m|^fatal: Read from socket failed: Connection reset by peer$|) {
 	} elsif ($message =~ m|^reverse mapping checking getaddrinfo|) {
 	} elsif ($message =~ m|^pam_succeed_if\(sshd\:auth\)\:|) {
