403b7a6253a69595f1550b0770ab21b1373381b8
Edward Z. Yang <ezyang@mit.edu>
2010-09-20 18:24:08 +0000

    Remove instructions obsoleted by kickstart.
    Legacy-ID: 1668

--- server/doc/install-howto.sh
+++ server/doc/install-howto.sh
@@ -1,4 +1,6 @@
 # This document is a how-to for installing a Fedora scripts.mit.edu server.
+# It is semi-vaguely in the form of a shell script, but is not really
+# runnable as it stands.
 
 set -e -x
 
@@ -13,56 +15,25 @@ alias asbuild="sudo -u scripts-build"
 # so it's been removed from the instructions.  If an install is hanging,
 # though, try adding NSS_NONLOCAL_IGNORE.
 
-[ -e /scripts-boot-count ] || echo 0 > /scripts-boot-count
-
 # This is actually just "pick an active scripts server".  It can't be
 # scripts.mit.edu because our networking config points that domain
 # at localhost, and if our server is not setup at that point things
 # will break.
 source_server="cats-whiskers.mit.edu"
 
-boot=${1:$(cat /scripts-boot-count)}
-
-# XXX: let 'branch' be the current svn branch you are on.  You want to
+# 'branch' is the current svn branch you are on.  You want to
 # use trunk if your just installing a new server, and branches/fcXX-dev
 # if your preparing a server on a new Fedora release.
+branch="trunk"
 
-doreboot() {
-    echo $(( $boot + 1 )) > /scripts-boot-count;
-    shutdown -r now "Rebooting for step $(cat /scripts-boot-count)"
-}
+# Start with a Scripts kickstarted install of Fedora (install-fedora)
 
-# Helper files for the install are located in server/fedora/config.
+# Take updates, reboot if there's a kernel update.
 
-# Start with a minimal install of Fedora.
-
-# Take updates
     yum update
-
-if [ $boot = 0 ]; then
-
-echo "--disabled" > /etc/sysconfig/system-config-firewall
-
-# Turn on network, so we can connect at boot
-chkconfig network on
-
-# Edit /etc/selinux/config so it has SELINUX=disabled and reboot.
-    sed -i 's/^SELINUX=.*/SELINUX=disabled/' /etc/selinux/config
-    doreboot
-fi
-
-if [ $boot = 1 ]; then
-# Create a scripts-build user account, and set up rpm to build in 
-# $HOME by doing a 
-# cp config/home/scripts-build/.rpmmacros /home/scripts-build/
-# (If you just use the default setup, it will generate packages 
-# in /usr/src/redhat.)
-    adduser scripts-build
 
 # Check out the scripts.mit.edu svn repository. Configure svn not to cache
 # credentials.
-
-    yum install -y subversion
 
     cd /srv
     # We must use an explicit source_server while setting up the Scripts
@@ -86,14 +57,9 @@ if [ $boot = 1 ]; then
 
 # Run "make install-deps" to install various prereqs.  Nonstandard
 # deps are in /mit/scripts/rpm.
-    yum install -y make
     make install-deps
     # You should pay close attention to the output of this command, and
     # note if packages you think should exist don't exist anymore.
-
-# Get some packages necessary for OpenAFS
-    yum install -y redhat-lsb
-    yum install -y autofs
 
 # Copy over root's dotfiles from one of the other machines.
 # Perhaps a useful change is to remove the default aliases
@@ -107,12 +73,6 @@ if [ $boot = 1 ]; then
     # will attempt to negotiate a server-to-server connection.
     # Instead, scp to your trusted machine as a temporary file,
     # and then push to the other server
-
-# Add scripts-build to the group 'mock'
-    usermod -a -G mock scripts-build
-
-# Install bind
-    yum install -y bind
 
 # Check out the scripts /etc configuration
     cd /root
@@ -131,17 +91,22 @@ if [ $boot = 1 ]; then
 # lines).  However, you can also temporarily install krb5 and setup the
 # keytabs and k5login to get Kerberized authentication.
 
-    service named start
-    chkconfig named on
+# Make sure network is working.  If this is a new server name, you'll
+# need to add it to /etc/hosts and
+# /etc/sysconfig/network-scripts/route-eth1.  Kickstart should have
+# configured eth0 and eth1 correctly; use service network restart
+# to add the new routes in route-eth1.
+    route
+    ifconfig
+    cat /etc/hosts
+    cat /etc/sysconfig/network-scripts/route-eth1
+    service network restart
 
 # This is the point at which you should start updating scriptsified
 # packages for a new Fedora release.  Consult 'upgrade-tips' for more
 # information.
 
     yum install -y scripts-base
-
-# Reload the iptables config to take down the restrictive firewall 
-    service iptables restart
 
 # Check that fs sysname is correct.  You should see, among others,
 # 'amd64_fedoraX_scripts' (vary X) and 'scripts'. If it's not, you
@@ -152,14 +117,6 @@ if [ $boot = 1 ]; then
     rpm -e --nodeps rsyslog
     yum install -y syslog-ng
     chkconfig syslog-ng on
-
-# Install various dependencies of the scripts system, including
-# glibc-devel.i586 (ezyang: already installed for me),
-# python-twisted-core (ditto), mod_fcgid, nrpe, nagios-plugins-all.
-    yum install -y mod_fcgid
-    yum install -y nrpe
-    yum install -y nagios-plugins-all
-    yum install -y fprintd-pam
 
 # Fix the openafs /usr/vice/etc <-> /etc/openafs mapping.
     echo "/afs:/usr/vice/cache:10000000" > /usr/vice/etc/cacheinfo
@@ -239,20 +196,24 @@ perldoc -u perllocal | grep head2 | cut -f 3 -d '<' | cut -f 1 -d '|' | sort -u 
 #   Pass -Z to easy_install to install them unzipped, as some zipped eggs
 #   want to be able to write to ~/.python-eggs.  (Also makes sourcediving
 #   easier.)
+    cat /usr/lib/python2.6/site-packages/easy-install.pth
 # - Look at `gem list` for Ruby gems.
 #   Again, use 'yum search' and prefer RPMs, but failing that, 'gem install'.
 #       ezyang: rspec-rails depends on rspec, and will override the Yum
 #       package, so... don't use that RPM yet
+    gem list
 # - Look at `pear list` for Pear fruits (or whatever they're called).
 #   Yet again, 'yum search' for RPMs before resorting to 'pear install'.  Note
 #   that for things in the beta repo, you'll need 'pear install package-beta'.
 #   (you might get complaints about the php_scripts module; ignore them)
+    pear list
 # - Look at `pecl list` for PECL things.  'yum search', and if you must,
 #   'pecl install' needed items. If it doesn't work, try 'pear install
 #   pecl/foo' or 'pecl install foo-beta' or those two combined.
-    # Automating this... will require a lot of batonning between
-    # the servers. Probably best way to do it is to write an actual
-    # script.
+    pecl list
+# Automating this... will require a lot of batonning between
+# the servers. Probably best way to do it is to write an actual
+# script.
 
 # Setup some Python config
     echo 'import site, os.path; site.addsitedir(os.path.expanduser("~/lib/python2.6/site-packages"))' > /usr/lib/python2.6/site-packages/00scripts-home.pth
@@ -316,9 +277,8 @@ perldoc -u perllocal | grep head2 | cut -f 3 -d '<' | cut -f 1 -d '|' | sort -u 
 # (daemon.scripts). Change this:
     vim /home/afsagent/renew # replace all mentions of daemon.scripts.mit.edu
 
-# Install 389-ds-base and set up replication (see ./install-ldap).
-    yum install 389-ds-base
-    # [complicated procedure here]
+# Set up replication (see ./install-ldap).
+    cat install-ldap
 
 # Make the services dirsrv, nslcd, nscd, postfix, and httpd start at
 # boot. Run chkconfig to make sure the set of services to be run is
@@ -355,12 +315,6 @@ perldoc -u perllocal | grep head2 | cut -f 3 -d '<' | cut -f 1 -d '|' | sort -u 
 
 # ThisCell got clobbered, replace it with athena.mit.edu
     echo "athena.mit.edu" > /usr/vice/etc/ThisCell
-
-# Kill unnecessary services. (It's probably good form to look through
-# `chkconfig | grep on` manually)
-    for i in avahi-daemon isdn nfslock pcscd rpcbind rpcgssd rpcidmapd; do
-        chkconfig "$i" off
-    done
 
 # Reboot the machine to restore a consistent state, in case you
 # changed anything. (Note: Starting kdump fails (this is ok))
