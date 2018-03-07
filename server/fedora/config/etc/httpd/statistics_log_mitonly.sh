027f38017e917ea2f18fcc13c021ee0ae02e2eb5
Anders Kaseorg <andersk@mit.edu>
2018-03-07 01:37:24 -0500

    Transitional configuration for new IP addresses on SIPB-486
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- server/fedora/config/etc/httpd/statistics_log_mitonly.sh
+++ server/fedora/config/etc/httpd/statistics_log_mitonly.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 perl -ne 'BEGIN { $| = 1 }
 next unless /^18\./;
-next if /^18\.181\./;
+next if /^18\.181\.|^18\.4\.86\./;
 chomp; split;
 if ($_[1] eq "scripts.mit.edu" && $_[2] =~ m|/(~[^/]+)/|) {
 print "$1\n";
 } else {
 print "$_[1]\n";
 }' >> /var/log/httpd/statistics_log
-#awk '/^18\./ && ! /^18.181/ { print $2; fflush() }' >> /var/log/httpd/statistics_log
+#awk '/^18\./ && ! /^18\.181\.|^18\.4\.86\./ { print $2; fflush() }' >> /var/log/httpd/statistics_log
