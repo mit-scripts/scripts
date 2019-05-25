58334520ed75df50601b348f6cdf729fbd1ac101
Quentin Smith <quentin@mit.edu>
2019-05-25 15:47:11 -0400

    Show diffs when running Ansible

--- server/fedora/ansible-config-me.sh
+++ server/fedora/ansible-config-me.sh
@@ -4,6 +4,6 @@ set -e
 set -x
 
 cd /srv/repository/ansible
-ansible-playbook playbook.yml -c local -l "localhost,$(hostname -f),$(hostname -s),127.0.0.1"
+ansible-playbook playbook.yml -c local -l "localhost,$(hostname -f),$(hostname -s),127.0.0.1" --diff
 
 systemctl disable ansible-config-me.service
