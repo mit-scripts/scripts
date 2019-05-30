5f8ae51b4f5e5fc23f78f38e085f0ca92c2124d0
Quentin Smith <quentin@mit.edu>
2019-05-29 23:14:08 -0400

    Write detailed logs of Ansible activity

--- server/fedora/ansible-config-me.sh
+++ server/fedora/ansible-config-me.sh
@@ -4,6 +4,6 @@ set -e
 set -x
 
 cd /srv/repository/ansible
-ansible-playbook playbook.yml -c local -l "localhost,$(hostname -f),$(hostname -s),127.0.0.1" --diff
+ansible-playbook playbook.yml -c local -l "localhost,$(hostname -f),$(hostname -s),127.0.0.1" --diff -v
 
 systemctl disable ansible-config-me.service
