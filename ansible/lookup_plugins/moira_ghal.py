df6f5f5138f0fed1535c1ff1ed8ce8ef35401dec
Quentin Smith <quentin@mit.edu>
2019-07-15 21:42:52 -0400

    Generate ssh_known_hosts automatically

---
+++ ansible/lookup_plugins/moira_ghal.py
@@ -0,0 +1,44 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+DOCUMENTATION = """
+lookup: moira_ghal
+ description:
+ - This lookup returns the aliases of a Moira host.
+"""
+
+import subprocess
+
+from ansible.plugins.lookup import LookupBase
+from ansible.utils.display import Display
+
+display = Display()
+
+DOMAIN = ".mit.edu"
+
+def ghal(host):
+    p = subprocess.Popen(
+        ["qy", "-n", "-s", "ghal", "*", host],
+        cwd=self._loader.get_basedir(),
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE)
+    (stdout, stderr) = p.communicate()
+    if stderr != "":
+        display.warning("qy: %s" % stderr)
+    return [s.split(',', 2)[0].lower() for s in stdout.splitlines()]
+
+class LookupModule(LookupBase):
+        def run(self, terms, include_short_names=False, include_cname=False, **kwargs):
+            ret = set()
+            for host in terms:
+                host = host.lower()
+                display.debug("Looking up aliases for: %s" % host)
+
+                ret += ghal(host)
+                if include_cname:
+                    ret.add(host)
+            if include_short_names:
+                for h in set(ret):
+                    if h.endswith(DOMAIN):
+                        ret.add(h[:-len(DOMAIN)])
+            return ret
