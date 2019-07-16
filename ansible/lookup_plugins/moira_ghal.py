19d792cda6e3a7c3b3b65a22b02cb23ec0f4969b
Quentin Smith <quentin@mit.edu>
2019-07-16 02:42:17 -0400

    ghal needs to be a method

--- ansible/lookup_plugins/moira_ghal.py
+++ ansible/lookup_plugins/moira_ghal.py
@@ -16,29 +16,28 @@ display = Display()
 
 DOMAIN = ".mit.edu"
 
-def ghal(host):
-    p = subprocess.Popen(
-        ["qy", "-n", "-s", "ghal", "*", host],
-        cwd=self._loader.get_basedir(),
-        stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE)
-    (stdout, stderr) = p.communicate()
-    if stderr != "":
-        display.warning("qy: %s" % stderr)
-    return [s.split(',', 2)[0].lower() for s in stdout.splitlines()]
-
 class LookupModule(LookupBase):
-        def run(self, terms, include_short_names=False, include_cname=False, **kwargs):
-            ret = set()
-            for host in terms:
-                host = host.lower()
-                display.debug("Looking up aliases for: %s" % host)
+    def ghal(self, host):
+        p = subprocess.Popen(
+            ["qy", "-n", "-s", "ghal", "*", host],
+            cwd=self._loader.get_basedir(),
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE)
+        (stdout, stderr) = p.communicate()
+        if stderr != "":
+            display.warning("qy: %s" % stderr)
+        return [s.split(',', 2)[0].lower() for s in stdout.splitlines()]
+    def run(self, terms, include_short_names=False, include_cname=False, **kwargs):
+        ret = set()
+        for host in terms:
+            host = host.lower()
+            display.debug("Looking up aliases for: %s" % host)
 
-                ret += ghal(host)
-                if include_cname:
-                    ret.add(host)
-            if include_short_names:
-                for h in set(ret):
-                    if h.endswith(DOMAIN):
-                        ret.add(h[:-len(DOMAIN)])
-            return ret
+            ret += self.ghal(host)
+            if include_cname:
+                ret.add(host)
+        if include_short_names:
+            for h in set(ret):
+                if h.endswith(DOMAIN):
+                    ret.add(h[:-len(DOMAIN)])
+        return ret
