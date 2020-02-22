0aac03f93bc0afea0aef0cef6b4d27d9637a9151
Quentin Smith <quentin@mit.edu>
2020-02-21 23:42:26 -0500

    Provide an ansible-config-me binary on PATH to trigger an Ansible run

--- server/fedora/ansible-config-me.sh
+++ server/fedora/ansible-config-me.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-set -e
-set -x
-
-cd /srv/repository/ansible
-ansible-playbook playbook.yml -c local -l "localhost,$(hostname -f | tr '[:upper:]' '[:lower:]'),$(hostname -s | tr '[:upper:]' '[:lower:]'),127.0.0.1" --diff -v
-
-touch /etc/ansible-config-done
+exec /srv/repository/ansible/roles/ansible-config-me/files/ansible-config-me --direct
