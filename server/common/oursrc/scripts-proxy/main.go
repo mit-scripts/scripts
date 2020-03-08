879aa1083ca72d96c7bbac70bde481ecb84fa67f
Quentin Smith <quentin@mit.edu>
2020-03-08 08:21:03 -0400

    Spoof client source IP

--- server/common/oursrc/scripts-proxy/main.go
+++ server/common/oursrc/scripts-proxy/main.go
@@ -8,25 +8,28 @@ import (
 	"net"
 	"strings"
 
+	"github.com/mit-scripts/scripts/server/common/oursrc/scripts-proxy/ldap"
 	"inet.af/tcpproxy"
-
-	ldap "gopkg.in/ldap.v3"
 )
 
 var (
 	httpAddrs   = flag.String("http_addrs", "0.0.0.0:80", "comma-separated addresses to listen for HTTP traffic on")
 	sniAddrs    = flag.String("sni_addrs", "0.0.0.0:443,0.0.0.0:444", "comma-separated addresses to listen for SNI traffic on")
-	ldapServer  = flag.String("ldap_server", "scripts-ldap.mit.edu:389", "LDAP server to query")
+	ldapServers = flag.String("ldap_servers", "scripts-ldap.mit.edu:389", "comma-spearated LDAP servers to query")
 	defaultHost = flag.String("default_host", "scripts.mit.edu", "default host to route traffic to if SNI/Host header cannot be parsed or cannot be found in LDAP")
 	baseDn      = flag.String("base_dn", "ou=VirtualHosts,dc=scripts,dc=mit,dc=edu", "base DN to query for hosts")
+	localRange  = flag.String("local_range", "18.4.86.0/24", "IP block for client IP spoofing. If the resolved destination address is in this subnet, the source IP address of the backend connection will be spoofed to match the client IP. This subnet needs to be local to the proxy.")
 )
+
+const ldapRetries = 3
 
 func always(context.Context, string) bool {
 	return true
 }
 
 type ldapTarget struct {
-	ldap *ldap.Conn
+	localPoolRange *net.IPNet
+	ldap           *ldap.Pool
 }
 
 // HandleConn is called by tcpproxy after receiving a connection and sniffing the host.
@@ -36,60 +39,60 @@ func (l *ldapTarget) HandleConn(netConn net.Conn) {
 	var pool string
 	var err error
 	if conn, ok := netConn.(*tcpproxy.Conn); ok {
-		pool, err = l.resolvePool(conn.HostName)
+		pool, err = l.ldap.ResolvePool(conn.HostName)
 		if err != nil {
 			log.Printf("resolving %q: %v", conn.HostName, err)
 		}
 	}
 	if pool == "" {
-		pool, err = l.resolvePool(*defaultHost)
+		pool, err = l.ldap.ResolvePool(*defaultHost)
 		if err != nil {
 			log.Printf("resolving default pool: %v", err)
 		}
 	}
+	// TODO: Serve an error page? Forward to scripts-director?
 	if pool == "" {
 		netConn.Close()
 		return
 	}
 	laddr := netConn.LocalAddr().(*net.TCPAddr)
+	destAddrStr := net.JoinHostPort(pool, fmt.Sprintf("%d", laddr.Port))
+	destAddr, err := net.ResolveTCPAddr("tcp", destAddrStr)
+	if err != nil {
+		netConn.Close()
+		log.Printf("parsing pool address %q: %v", pool, err)
+		return
+	}
 	dp := &tcpproxy.DialProxy{
-		Addr: fmt.Sprintf("%s:%d", pool, laddr.Port),
-		// TODO: Set DialContext to override the source address
+		Addr: destAddrStr,
+	}
+	if l.localPoolRange.Contains(destAddr.IP) {
+		raddr := netConn.RemoteAddr().(*net.TCPAddr)
+		sourceAddr := &net.TCPAddr{
+			IP: raddr.IP,
+		}
+		dp.DialContext = func(ctx context.Context, network, address string) (net.Conn, error) {
+			return net.DialTCP(network, sourceAddr, destAddr)
+		}
 	}
 	dp.HandleConn(netConn)
-}
-
-func (l *ldapTarget) resolvePool(hostname string) (string, error) {
-	escapedHostname := ldap.EscapeFilter(hostname)
-	req := ldap.NewSearchRequest(
-		*baseDn,
-		ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
-		fmt.Sprintf("(|(scriptsVhostName=%s)(scriptsVhostAlias=%s))", escapedHostname, escapedHostname),
-		[]string{"scriptsVhostPoolIPv4"},
-		nil,
-	)
-	sr, err := l.ldap.Search(req)
-	if err != nil {
-		return "", err
-	}
-	for _, entry := range sr.Entries {
-		return entry.GetAttributeValue("scriptsVhostPoolIPv4"), nil
-	}
-	// Not found is not an error
-	return "", nil
 }
 
 func main() {
 	flag.Parse()
 
-	l, err := ldap.Dial("tcp", *ldapServer)
+	_, ipnet, err := net.ParseCIDR(*localRange)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer l.Close()
+
+	ldapPool := ldap.NewPool(strings.Split(*ldapServers, ","), *baseDn, ldapRetries)
 
 	var p tcpproxy.Proxy
-	t := &ldapTarget{ldap: l}
+	t := &ldapTarget{
+		localPoolRange: ipnet,
+		ldap:           ldapPool,
+	}
 	for _, addr := range strings.Split(*httpAddrs, ",") {
 		p.AddHTTPHostMatchRoute(addr, always, t)
 	}
